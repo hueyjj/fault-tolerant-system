11932f1783bd733fc991866914107a22a351a2d4
Jorge Henriquez <joahenri@ucsc.edu>
2018-10-19 17:13:17 -0700

    Wrote store package

---
+++ store/store.go
@@ -0,0 +1,40 @@
+package store
+
+import (
+	"fmt"
+)
+
+type Store struct {
+	keyvalMap map[string]string
+}
+
+func New() *Store {
+	return &Store{
+		keyvalMap: make(map[string]string),
+	}
+}
+
+// Put places val into key, and returns true
+// if the value was replaced
+func (s *Store) Put(key, val string) bool {
+	val, exists := s.keyvalMap[key]
+	s.keyvalMap[key] = val
+	return exists
+}
+
+func (s *Store) Get(key string) (string, error) {
+	val, exists := s.keyvalMap[key]
+	if !exists {
+		return "", fmt.Errorf("key %s does not exit in the map", key)
+	}
+	return val, nil
+}
+
+func (s *Store) Delete(key string) error {
+	_, exists := s.keyvalMap[key]
+	if !exists {
+		return fmt.Errorf("key %s does not exit in the map", key)
+	}
+	delete(s.keyvalMap, key)
+	return nil
+}
