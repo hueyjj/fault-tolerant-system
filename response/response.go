98a15adc86cef2fb0146664087422371394f6e48
Jorge Henriquez <joahenri@ucsc.edu>
2018-10-25 23:45:03 -0700

    fix compilation error

--- response/response.go
+++ response/response.go
@@ -3,7 +3,7 @@ package response
 type Response struct {
 	Replaced bool   `json:"replaced,omitempty"`
 	Msg      string `json:"msg,omitempty"`
-	Value    int    `json:"value,omitempty"`
+	Value    string `json:"value,omitempty"`
 	Result   string `json:"result,omitempty"`
 	IsExists bool   `json:"isExists,omitempty"`
 }
